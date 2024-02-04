efebb66ba018855b3d05faf8153166b3c2519560
Zango99 <126648533+Zango99@users.noreply.github.com>
2024-02-05 11:24:39 +1300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test
+{
+    public static void main(String arg[]){
+
+	int tableNo = 5;
+		
+	for (i = 1; i<=5; i++){
+	
+	System.out.println(tableNo + " * "+ i + " = " + tableNo *i);
+	}
+
+     }
+ 
+}
